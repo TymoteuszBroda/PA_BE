961dd0e3165d8ef6152f937c407f2d86ac3f4fa6
tbroda <tymoteusz.91100@gmail.com>
2025-03-29 10:59:52 +0100

    added license assignment funtcionalty

--- obj/Debug/net9.0/PermAdminAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/PermAdminAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermAdminAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb83ad276b82d71a823698b50a0b3bc15a3e659a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d9f13abfc01485034d0035bb8a28dd90aacf42")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermAdminAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermAdminAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
