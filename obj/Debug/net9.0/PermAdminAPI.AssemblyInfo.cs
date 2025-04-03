df2fd8eceedd5fe47578e0f8fa32da6fc99bbade
tbroda <tymoteusz.91100@gmail.com>
2025-04-03 20:01:26 +0200

    merge prep

--- obj/Debug/net9.0/PermAdminAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/PermAdminAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermAdminAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e41b151c69a60ce90349a86917988f0c06ab170")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8693abeb1c268420f529764ee77f5e49fc1fbf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermAdminAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermAdminAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
